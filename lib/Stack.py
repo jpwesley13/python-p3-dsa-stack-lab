6162972a7f4478ca3dabf2a91f4524b14479875b
Jared Patterson <jpwesley34@gmail.com>
2024-05-15 14:57:49 -0700

    Passes tests with help.

--- lib/Stack.py
+++ lib/Stack.py
@@ -1,25 +1,36 @@
 class Stack:
 
     def __init__(self, items = [], limit = 100):
-        pass
+        self.items = items
+        self.limit = limit
 
     def isEmpty(self):
-        pass
+        return self.items == []
 
     def push(self, item):
-        pass
+        if not self.full(): # Important to make sure stack is not full before adding more.
+            self.items.append(item)
+        else:
+            return None # And if it is, then return None.
 
     def pop(self):
-        pass
+        if len(self.items) > 0:
+            return self.items.pop()
+        return None
 
     def peek(self):
         pass
     
     def size(self):
-        pass
+        return len(self.items)
 
     def full(self):
-        pass
+        if len(self.items) == self.limit:
+            return True
+        return False
 
     def search(self, target):
-        pass
+        for i in reversed(range(len(self.items))): # For each reversed number in the range of the length of the items list. Reverse the order is just for efficiency's sake, I think.
+            if self.items[i] == target: # If the item at the i'th index is the target
+                return len(self.items) -1 -i # Give me the length of the items list minus one minus that i index where the target was found.
+        return -1
